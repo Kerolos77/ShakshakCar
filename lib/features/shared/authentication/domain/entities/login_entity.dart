a868fb460b5cff252f562e62c71f82d7e90b8688
kerolos_faie <71794099+Kerolos77@users.noreply.github.com>
2026-08-12 00:36:32 +0300

    fix: resolve LoginModel parsing type error when API returns Map for data

--- lib/features/shared/authentication/domain/entities/login_entity.dart
+++ lib/features/shared/authentication/domain/entities/login_entity.dart
@@ -3,11 +3,13 @@ class LoginEntity {
   final String? msg;
   final int? status;
   final bool? statusval;
+  final String? otp;
 
   const LoginEntity({
     this.data,
     this.msg,
     this.status,
     this.statusval,
+    this.otp,
   });
 }
